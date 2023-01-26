bd06649fe4f002d1e5002bf7f9dc0a2c74a28671
Jonassavas <jonas.savas123@gmail.com>
2023-01-26 15:47:18 +0100

    Adds an initial test file for JUnit

---
+++ Lab1_decide/src/DecideTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+
+
+public class DecideTest{
+
+    //Example: Typical test syntax
+    @Test
+    public void fooTest(){
+
+
+
+    }
+
+}
