e4c18e16bb9c42ec882c510a673d2eddeaa3cc14
samuelfa <samuelfa@kth.se>
2023-01-28 15:24:52 +0100

    Add imple `Point` class
    Add a point class with basic methods for addition
    subtraction and distance.

---
+++ src/main/java/com/decide/Point.java
@@ -0,0 +1,25 @@
+package com.decide;
+
+import java.lang.Math;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point add(Point rhs) {
+        return new Point(this.x + rhs.x, this.y + rhs.y);
+    }
+
+    public Point sub(Point rhs) {
+        return new Point(this.x - rhs.x, this.y - rhs.y);
+    }
+
+    public double dist(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}
