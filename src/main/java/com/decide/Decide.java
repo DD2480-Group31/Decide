4716fa1da20bc7b9cd17473304332e89f86c24d9
sebvei <54596049+sebvei@users.noreply.github.com>
2023-01-29 16:08:52 +0100

    Changed quadrants 3 and 4.

--- src/main/java/com/decide/Decide.java
+++ src/main/java/com/decide/Decide.java
@@ -218,8 +218,8 @@ class Decide{
 	private int getQuadrant(double x, double y){
 		if(x >= 0 && y >= 0) return 1;
 		if(x < 0 && y >= 0) return 2;
-		if(x >= 0 && y < 0) return 3;
-		if(x < 0 && y < 0) return 4;
+		if(x <= 0 && y < 0) return 3;
+		if(x > 0 && y < 0) return 4;
 
 		return -1;
 	}
